568b21c208b86c03cd583e82534f0cf5eb13f679
Fabian <fabiancotic93@gmail.com>
2017-12-27 16:25:33 +0100

    Minor refactoring

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,5 @@
 public class Application {
     public static void main(String[] args){
-        System.out.print("Fuck the lüäH");
+        System.out.print("The Ding goes scraaap pap pap pap kak kak - skubidabu bab and a bumm burrr BUMMM");
     }
 }
